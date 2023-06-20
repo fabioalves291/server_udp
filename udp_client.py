c003dd9a788c3a6cb250aaa989f548f7829c2c89
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2023-06-19 21:55:35 -0300

    alto

--- udp_client.py
+++ udp_client.py
@@ -1,6 +1,7 @@
 import socket   
 import sys
 import os
+import time
 
 def menu():
     print('''
@@ -27,46 +28,49 @@ codepage    = 'UTF-8'
 buffersize  = 512
 menu()
 while True:
-    input_menu = input(': ')
-    if input_menu == '\\help':
-        print(menu())
-        continue
-    socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socket_client.sendto(input_menu.encode(codepage),(ipserver,portserver))
-    msg,adress = socket_client.recvfrom(buffersize)
     try:
-        msgdecode = msg.decode(codepage)
-    except UnicodeDecodeError:
-        pass
-    if input_menu =='\\f':
-        msgdecode = msgdecode.replace('[',' ')
-        msgdecode = msgdecode.replace(']',' ')
-        listmsg = msgdecode.split(',')
+        input_menu = input(': ')
+        if input_menu == '\\help':
+            print(menu())
+            continue
+        socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_client.sendto(input_menu.encode(codepage),(ipserver,portserver))
+        msg,adress = socket_client.recvfrom(buffersize)
+        try:
+            msgdecode = msg.decode(codepage)
+        except UnicodeDecodeError:
+            pass
+        if input_menu =='\\f':
+            msgdecode = msgdecode.replace('[',' ')
+            msgdecode = msgdecode.replace(']',' ')
+            listmsg = msgdecode.split(',')
 
-        for dados in listmsg:
-            print(dados.replace("'",' '))
-    
-    elif input_menu[:3] == '\\f:':
-        file = open(fr'arquivos_cliente/{input_menu[3:]}','wb') 
-        inicio = True
+            for dados in listmsg:
+                print(dados.replace("'",' '))
         
-        while inicio:
-            msg = socket_client.recv(buffersize)
-            #print(msg)
-            try:
-                if msg.decode(codepage) =='f':
-                    inicio = False
-            except UnicodeDecodeError:
-                pass
-            file.write(msg)
-        file.close()
-        print('arquivo recebido')
-    elif input_menu == '\\exit':
-        socket_client.close()
-        input('Enter para continuar')
-        quit()
-    else:
-        print(msgdecode)
-
-    
-    sair()
+        elif input_menu[:3] == '\\f:':
+            file = open(fr'arquivos_cliente/{input_menu[3:]}','wb') 
+            inicio = True
+            
+            while inicio:
+                msg = socket_client.recv(buffersize)
+                print(msg)
+                try:
+                    if msg.decode(codepage) =='f':
+                        inicio = False
+                except UnicodeDecodeError:
+                    pass
+                file.write(msg)
+            file.close()
+            print('arquivo recebido')
+        elif input_menu == '\\exit':
+            socket_client.close()
+            input('Enter para continuar')
+            quit()
+        else:
+            print(msgdecode)
+        sair()
+    except:
+        continue
+        print("err")
+        time.sleep(2)
--- udp_servidor_t.py
+++ udp_servidor_t.py
@@ -1,6 +1,7 @@
 import socket 
 import os
 import sys
+import time
 
 def datalist():
     totallist       = []
@@ -23,37 +24,49 @@ portserver          = 2000
 buffersize          = 512
 
 while True:
-    socket_server   = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    socket_server.bind((ipserver, portserver))
-    print('servidor aberto:')
-    data,adress     = socket_server.recvfrom(buffersize)
-    data            = data.decode(codepage)
-    print(adress,data)
-    print(data[:3])
-   
-    if data =='\\f':
-        print(adress,'solicita','Datalist')
-        datasfiles  = str(datalist())
-        socket_server.sendto(datasfiles.encode(codepage),adress)
+    try:
+        socket_server   = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        try:
+            socket_server.bind((ipserver, portserver))
+        except:
+            print("> servidor interrompido!\n> error na porta ou ip escolhido")
+            time.sleep(4)
+            continue
+        print('servidor aberto:')
+        data,adress     = socket_server.recvfrom(buffersize)
+        data            = data.decode(codepage)
+        print(adress,data)
+        print(data[:3])
+       
+        if data =='\\f':
+            print(adress,'solicita','Datalist')
+            datasfiles  = str(datalist())
+            socket_server.sendto(datasfiles.encode(codepage),adress)
 
-    elif data == '\\exit':
-        print(adress,'fecha os sistema client e server')
-        socket_server.sendto('desligando client e servidor'.encode(codepage),adress)
-        socket_server.close()
-        quit()
+        elif data == '\\exit':
+            print(adress,'fecha os sistema client e server')
+            socket_server.sendto('desligando client e servidor'.encode(codepage),adress)
+            socket_server.close()
+            quit()
+                
+        elif data[:3] == '\\f:':
+            file        = open(fr'arquivos_servidor/{data[3:]}','rb')   
+            socket_server.sendto('arquivo inexistente'.encode(codepage),adress)
+            print(adress,'arquivo solicitado inexistente')
             
-    elif data[:3] == '\\f:':
-        file        = open(fr'arquivos_servidor/{data[3:]}','rb')   
-        socket_server.sendto('arquivo inexistente'.encode(),adress)
-        print(adress,'arquivo solicitado inexistente')
-        
-        filebits    = file.read(buffersize)
-        while filebits: 
-            print(filebits)
-            socket_server.sendto(filebits,adress)
             filebits    = file.read(buffersize)
-        file.close()
-        socket_server.sendto('f'.encode(),adress)
-    else:
-        print(adress,'solicita opção inválida')
-        socket_server.sendto('opção invalida. Use o comando \help '.encode(),adress)   
+            while filebits: 
+                print(filebits)
+                socket_server.sendto(filebits,adress)
+                filebits    = file.read(buffersize)
+            file.close()
+            socket_server.sendto('f'.encode(codepage),adress)
+        else:
+            print(adress,'solicita opção inválida')
+            socket_server.sendto('opção invalida. Use o comando \help '.encode(),adress)   
+   
+        
+    except:
+        socket_server.sendto('data_lixo pass'.encode(codepage),adress)
+        socket_server.sendto('f'.encode(codepage),adress)
+        print("error")
